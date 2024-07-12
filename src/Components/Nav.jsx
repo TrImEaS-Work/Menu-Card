c4b515b6255a2634acd7f5c4f2ce081edc5aa514
TrImEaS <trimeas@hotmail.com>
2024-07-11 22:19:04 -0300

    add

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,11 @@
 import { NavLink, useLocation } from "react-router-dom"
 import logo from '../assets/icon.jpg'
-import { FaInstagram, FaWhatsapp } from "react-icons/fa"
+import { FaInstagram, FaWhatsapp, FaBars, FaTimes } from "react-icons/fa"
+import { useState } from "react"
 
 export default function Nav() {
   const path = useLocation().pathname
+  const [mobileMenu, setMobileMenu] = useState(false)
 
   return (
     <section className="flex justify-between items-center px-10 w-full h-20 bg-[#292929] border-b">
@@ -11,7 +13,7 @@ export default function Nav() {
         <NavLink to='/'><img src={logo} className="rounded-full object-contain duration-300"/></NavLink>
       </article>
 
-      <article>
+      <article className="max-sm:hidden block">
         <ul className="flex w-fit px-2 h-full gap-x-10 justify-center items-center text-[#fafafa]">
           <li><NavLink to='/' className={`${path === '/' ? 'isActive' : ''} border-b-2 border-transparent white-shadow-hover duration-300`}>Home</NavLink></li>
           <li><NavLink to='/precios' className={`${path === '/precios' ? 'isActive' : ''} border-b-2 border-transparent white-shadow-hover duration-300`}>Precios</NavLink></li>
@@ -22,6 +24,23 @@ export default function Nav() {
           </ul>
         </ul>
       </article>
+      
+      <article className='hidden relative text-white text-3xl max-sm:flex'>
+        <FaBars onClick={()=> setMobileMenu(!mobileMenu)}/>
+
+        <ul className={`${mobileMenu ? 'left-20 hidden' : 'left-[-110px]'} flex bg-[#292929] absolute flex-col text-2xl w-[180px] px-2 h-[50vh] top-[-25px] gap-10 justify-center items-center text-[#fafafa] z-20 rounded-md border duration-300`}>
+          <li><NavLink onClick={()=> setMobileMenu(!mobileMenu)} to='/' className={`${path === '/' ? 'isActive' : ''} border-b-2 border-transparent white-shadow-hover duration-300`}>Home</NavLink></li>
+          <li><NavLink onClick={()=> setMobileMenu(!mobileMenu)} to='/precios' className={`${path === '/precios' ? 'isActive' : ''} border-b-2 border-transparent white-shadow-hover duration-300`}>Precios</NavLink></li>
+          <li><NavLink onClick={()=> setMobileMenu(!mobileMenu)} to='/contacto' className={`${path === '/contacto' ? 'isActive' : ''} border-b-2 border-transparent white-shadow-hover duration-300`}>Contacto</NavLink></li>
+          <ul className="flex gap-x-5 text-3xl">
+            <a onClick={()=> setMobileMenu(!mobileMenu)} href={`https://wa.me/541133690584?text=Hola me comunico para consultar acerca de las cartas digitales, mi nombre es .`} target="_blank"><FaWhatsapp className="text-white white-shadow-hover duration-300"/></a>
+            <a onClick={()=> setMobileMenu(!mobileMenu)} href="https://www.instagram.com/_trimeas_/" target="_blank"><FaInstagram className="text-white white-shadow-hover duration-300"/></a>
+          </ul>
+        </ul>
+      </article>
+
+      <FaTimes onClick={()=> setMobileMenu(!mobileMenu)} className={`${mobileMenu ? 'hidden' : 'block'} text-white text-3xl absolute right-3 top-3 z-20`}/>
+      
     </section>
   )
 }
--- src/Components/Prices.jsx
+++ src/Components/Prices.jsx
@@ -2,11 +2,11 @@ import { FaCheck } from 'react-icons/fa'
 
 export default function Prices() {
   return (
-    <section className='w-full gap-12 flex-wrap min-h-[883px] text-gray-50 bg-[#111] flex justify-center pt-[140px] p-4'>
+    <section className='w-full gap-12 flex-wrap min-h-[883px] text-gray-50 bg-[#111] flex justify-center py-[50px] p-10'>
       <article className="new-shadow-cyan hover:scale-105 flex flex-col gap-5 new-shadow-cyan-hover duration-300 w-[400px] h-[400px] p-[28px] border rounded-xl">
         <p className='flex w-full justify-between'>
           <span className='font-bold text-2xl text-cyan-100'>Carta Basica</span>
-          <span className='font-medium text-2xl text-cyan-100'>$119.99</span>
+          <span className='font-medium text-2xl text-cyan-100'>USD119.99</span>
         </p>
         <p className='flex items-center gap-x-2'>
           <FaCheck className='text-cyan-200'/>
@@ -24,9 +24,9 @@ export default function Prices() {
           <FaCheck className='text-cyan-200'/>
           <span>Y mas cosas...</span>
         </p>
-        <div className='h-[70px] w-full flex items-end'>
+        <div className='h-[170px] pt-3 w-full flex items-end'>
           <a 
-            href='https://wa.me/541133690584?text=Hola me comunico para consultar acerca del paquete de carta basica, mi nombre es .' 
+            href='https://wa.me/541135790133?text=Hola me comunico para consultar acerca del paquete de carta basica, mi nombre es .' 
             className='font-bold px-3 py-2 rounded-xl new-shadow-cyan-hover hover:scale-105 duration-300 border w-fit h-fit text-cyan-100 border-cyan-100'>Contratar</a>
         </div>
       </article>
@@ -34,7 +34,7 @@ export default function Prices() {
       <article className="new-shadow-cyan hover:scale-105 flex flex-col gap-5 new-shadow-cyan-hover duration-300 w-[400px] h-[400px] p-[28px] border rounded-xl">
         <p className='flex w-full justify-between'>
           <span className='font-bold text-2xl text-cyan-100'>Carta Avanzada</span>
-          <span className='font-medium text-2xl text-cyan-100'>$189.99</span>
+          <span className='font-medium text-2xl text-cyan-100'>USD189.99</span>
         </p>
         <p className='flex items-center gap-x-2'>
           <FaCheck className='text-cyan-200'/>
@@ -52,9 +52,9 @@ export default function Prices() {
           <FaCheck className='text-cyan-200'/>
           <span>Y mas cosas...</span>
         </p>
-        <div className='h-[70px] w-full flex items-end'>
+        <div className='h-[170px] pt-3 w-full flex items-end'>
           <a 
-            href='https://wa.me/541133690584?text=Hola me comunico para consultar acerca del paquete de carta avanzada, mi nombre es .' 
+            href='https://wa.me/541135790133?text=Hola me comunico para consultar acerca del paquete de carta avanzada, mi nombre es .' 
             className='font-bold px-3 py-2 rounded-xl new-shadow-cyan-hover hover:scale-105 duration-300 border w-fit h-fit text-cyan-100 border-cyan-100'>Contratar</a>
         </div>
       </article>
@@ -85,7 +85,7 @@ export default function Prices() {
         </p>
         <div className='h-[70px] w-full flex items-end'>
           <a 
-            href='https://wa.me/541133690584?text=Hola me comunico para consultar acerca de el paquete personalizado para cartas digitales, mi nombre es ' 
+            href='https://wa.me/541135790133?text=Hola me comunico para consultar acerca de el paquete personalizado para cartas digitales, mi nombre es ' 
             className='font-bold px-3 py-2 rounded-xl new-shadow-cyan-hover hover:scale-105 duration-300 border w-fit h-fit text-cyan-100 border-cyan-100'>Contratar</a>
         </div>
       </article>
